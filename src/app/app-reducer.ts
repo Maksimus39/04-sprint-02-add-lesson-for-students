2e6767c250aaad231f397d8d8797cfbe298806fc
Macsim Minakov <maksim.minakov.83@mail.ru>
2025-01-12 12:53:26 +0300

    1.2 - "Loader: Решение"

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -9,9 +9,20 @@ type AppStateType = typeof initialState
 
 export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
   switch (action.type) {
+    case 'SET_APP_STATUS':{
+      return {
+        ...state,status: action.status
+      }
+    }
     default:
       return state
   }
 }
 
-type ActionsType = any
+type ActionsType = ReturnType<typeof setAppStatusAC>
+
+
+export const setAppStatusAC = (status: RequestStatusType) =>({
+  type: 'SET_APP_STATUS' as const,
+  status
+})
